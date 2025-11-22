6fb164138061bcba4229b01b1ac1c25f25699468
viktor barath <viktor.barath7@gmail.com>
2025-11-22 23:10:30 +0100

    Update financial_data.py updated code

--- financial_data.py
+++ financial_data.py
@@ -15,9 +15,34 @@ def collect_close_column_numbers(file):
             close_numbers.append(elements[4])
     return close_numbers[1:]
 
-def avarage_closing_price():
+close_num_list = collect_close_column_numbers(
+        'Download Data - STOCK_AT_XWBO_ANDR.csv')
+
+def avg_closing_price():
     """Calculate avg closing price."""
-    pass
+    sum_closing_price = 0
+    for price in close_num_list:
+        sum_closing_price += float(price)
+    avg_closing_price = sum_closing_price / len(close_num_list)
+
+    return avg_closing_price
 
 
-print(collect_close_column_numbers('Download Data - STOCK_AT_XWBO_ANDR.csv'))
+def check_last_price(last_price):
+    avg_closing = avg_closing_price()
+    if avg_closing < last_price:
+        indicator = 'above' #last price was above
+    elif avg_closing > last_price:
+        indicator = 'below' #last price was below
+    else:
+        indicator = 'the same as'
+
+    return indicator
+
+
+last_price = float(close_num_list[-1])
+
+print(f'The avargae closing price was {avg_closing_price():.2f}.')
+
+print(f'The most recent closing price {avg_closing_price():.2f} was '
+      f'{check_last_price(last_price)} the avarage.')
