86963b26339bf6a25d6d7420a16c9d43ab608b3d
viktor barath <viktor.barath7@gmail.com>
2025-11-22 23:11:43 +0100

    Update print_demand, made some tweaks

--- print_demand.py
+++ print_demand.py
@@ -1,18 +1,36 @@
 #!usr/bin/env python
 
 """
-..modulauther:: Viktor Barath <viktor.barath7@gmail.com>
+.. moduleauthor:: Viktor Barath <viktor.barath7@gmail.com>
 """
 
 import requests
-from bs4 import BeautifulSoup
 
-url = 'https://study.find-santa.eu/data/py/r101.txt'
-get_text = requests.get(url)
-html_content = get_text.text
+def get_data_from_webpage(url):
+    """Fetch data from given webpage"""
+    get_webpage = requests.get(url)
+    text = get_webpage.text.strip()
+    lines = text.splitlines()
 
-soup = BeautifulSoup(html_content, 'html.parser')
-texts = soup.get_text()
+    column_demand = []
 
+    for line in lines:
+        # Splits lines into elements, where whitespace is
+        parts = line.split()
+        # Assigne column demand only
+        column_demand.append(parts [3])
 
-print(data)
+    return column_demand
+
+def calc_total_demand():
+    """Sum numbers from demand"""
+    demand_lines = get_data_from_webpage('https://study.find-santa.eu/data/py/r101.txt')
+    total_demand = 0
+
+    for demand in demand_lines:
+        if not demand.isalpha():
+            total_demand += float(demand)
+
+    return total_demand
+
+print(f'Total demand: {calc_total_demand()}')
