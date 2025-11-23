802bade6aa7c28145d49ba2138913628bb047698
viktor barath <viktor.barath7@gmail.com>
2025-11-23 13:13:12 +0100

    Update rot13 file with finished code and dependency file

--- rot13.py
+++ rot13.py
@@ -3,13 +3,33 @@
 """
 .. moduleauthor:: Viktor Barath <viktor.barath7@gmail.com>
 """
+input_file = input('File to decode: ')
 
 def read_file(file):
     """Read file in and convert it all uppercase."""
-    pass
+    message = ''
+    with open(file) as f:
+        message += f.read()
 
-def rot13(message, direction = 1):
+    return message
+
+
+def rot13(message):
     """Encrypt, decrypt input text."""
-    for character in characters:
-        pass
+    offset = 13
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    translated_message = ''
 
+    for char in message.upper():
+        if not char.isalpha():
+            translated_message += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            translated_message += alphabet[new_index]
+
+    return translated_message
+
+message = read_file(input_file)
+
+print(f'Translated message:\n{rot13(message)}')
